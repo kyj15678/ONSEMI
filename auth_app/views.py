88acccc77e9c729c5a4901e87c1f565d664aa2ab
jaehong9809 <0915jhlee@daum.net>
2024-06-24 11:03:35 +0900

    feat: user model 생성

---
+++ auth_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+# 회원가입 -> template, 비밀번호가 2개가
+# 로그인 -> form에 아무것도 입력안했을 떄? 어떻게해야할지
+# 로그아웃 -> 1초
+
+# 계정확인  -> views -> templates로 context( views가 templates에게 데이터 전달 )-> templates를 꾸미기
+
+# 계정 수정 -> views -> templates로 context( views가 templates에게 데이터 전달 )-> 변경사항 꾸미기
+
+# 계정 삭제 -> 1초
