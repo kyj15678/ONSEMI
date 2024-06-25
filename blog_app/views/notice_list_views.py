170eab43b7adb3fad323784358f3b14c593a7189
Hyeok Heo <heo2922@naver.com>
2024-06-25 14:42:05 +0900

    feat : notice_list 개발 중

--- blog_app/views/notice_list_views.py
+++ blog_app/views/notice_list_views.py
@@ -1,22 +1,15 @@
-from blog_app.models import Blog
+from blog_app.models import *
 from django.shortcuts import render
 
-
-# 가정: notices 데이터는 데이터베이스 또는 다른 저장소에 저장되어 있음
-notices = [
-    {'id': 1, 'title': '공지 제목 1', 'date': '2024-06-24', 'content': '공지 내용 1...'},
-    {'id': 2, 'title': '공지 제목 2', 'date': '2024-06-23', 'content': '공지 내용 2...'},
-    {'id': 3, 'title': '공지 제목 3', 'date': '2024-06-22', 'content': '공지 내용 3...'},
-    {'id': 4, 'title': '공지 제목 4', 'date': '2024-06-21', 'content': '공지 내용 4...'},
-    {'id': 5, 'title': '공지 제목 5', 'date': '2024-06-20', 'content': '공지 내용 5...'},
-    {'id': 6, 'title': '공지 제목 6', 'date': '2024-06-19', 'content': '공지 내용 6...'},
-    {'id': 7, 'title': '공지 제목 7', 'date': '2024-06-18', 'content': '공지 내용 7...'},
-    {'id': 8, 'title': '공지 제목 8', 'date': '2024-06-17', 'content': '공지 내용 8...'},
-]
+def notice_all(request):
+    notice_list = Blog.objects.all()
+    context = {'notice_list': notice_list}
+    print(request.user)
+    return render(request, 'blog_app/notice_list.html', context)
 
 
-def notice_list(request):
-    #notices = Blog.objects.all() # test
-    return render(request, 'blog_app/notice_list.html', {'notices': notices})
-
-
+def search(request):
+    query = Blog.objects.filter(title__contains=request.GET.get('search'))
+    context = {'notice_list': query}
+    print(query)
+    return render(request, 'blog_app/notice_list.html', context)
